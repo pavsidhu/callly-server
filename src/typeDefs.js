d5b99096c93150ebc7657dcd239273c0e7a2b737
Pav Sidhu <pav@pavsidhu.com>
2018-01-24 18:25:51 +0000

    Add GraphQL types

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,12 +1,45 @@
 const typeDefs = `
+  type Query {
+    users: [User],
+    foods: [Food],
+    meals: [Meal]
+  }
+
   type User {
     id: ID!,
     firstName: String!,
     lastName: String!,
+    email: String!,
+    password: String,
+    facebook: FacebookAuth,
+    createdAt: String!,
+    updatedAt: String!
   }
 
-  type Query {
-    users: [User]
+  type FacebookAuth {
+    id: String,
+    token: String
+  }
+
+  type Food {
+    id: ID!
+    name: String!,
+    protein: Float!,
+    carbohydrates: Float!,
+    fats: Float!
+    manufacturer: String,
+    source: String!,
+    createdAt: String!,
+    updatedAt: String!
+  }
+
+  type Meal {
+    id: ID!,
+    name: String,
+    foods: [Food],
+    user: User,
+    createdAt: String!,
+    updatedAt: String!
   }
 `
 
