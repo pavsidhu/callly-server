a6853f63f1a4f90a06d7ffbfeebcad64bee5e2cc
Pav Sidhu <pav@pavsidhu.com>
2018-01-30 23:48:46 +0000

    Add resolvers for queries

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,21 @@
 import User from './models/User'
+import Meal from './models/Meal'
+import Food from './models/Food'
+import Manufacturer from './models/Manufacturer'
+import FoodSource from './models/FoodSource'
 
 const resolvers = {
   Query: {
     Users: async () => await User.findAll(),
-    User: async (_, { id }) => await User.findOne({ where: { id } })
+    User: async (_, { id }) => await User.findOne({ id }),
+    Meals: async () => await Meal.findAll(),
+    Meal: async (_, { id }) => await Meal.findOne({ id }),
+    Foods: async () => await Food.findAll(),
+    Food: async (_, { id }) => await Food.findOne({ id }),
+    Manufacturers: async () => await Manufacturers.findAll(),
+    Manufacturer: async (_, { id }) => await Manufacturer.findOne({ id }),
+    FoodSource: async () => await FoodSource.findAll(),
+    FoodSource: async (_, { id }) => await FoodSource.findOne({ id })
   },
   Mutation: {
     createUser: async (_, { firstName, lastName, email, password }) =>
